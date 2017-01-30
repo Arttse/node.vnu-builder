dca35a3444554ceb17aea336c9a30a78f86ea434
Nikita Bystrov <arttsesoft@gmail.com>
2017-01-30 21:17:13 +0300

    New test. Argument opts throws

--- test.js
+++ test.js
@@ -6,7 +6,19 @@ import isPlainObject from 'is-plain-object';
 import test from 'ava';
 import m from './';
 
-test ( 'Build vnu.jar with default options (jar)', t => {
+test ( 'Argument opts throws', async t => {
+
+  const msg = 'Options must be a plain object';
+
+  t.is ( (await t.throws ( m ( '' ) ) ).message, msg );
+  t.is ( (await t.throws ( m ( 1 ) ) ).message, msg );
+  t.is ( (await t.throws ( m ( true ) ) ).message, msg );
+  t.is ( (await t.throws ( m ( [] ) ) ).message, msg );
+  t.is ( (await t.throws ( m ( null ) ) ).message, msg );
+
+} );
+
+test ( 'Build vnu.jar with default options', t => {
 
   return m ()
     .then ( result => {
