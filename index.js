314dfb4a72e372b3343c1179b054a5d4b21c5f72
Nikita Bystrov <arttsesoft@gmail.com>
2017-01-30 11:13:12 +0300

    Build arguments moved to options

--- index.js
+++ index.js
@@ -98,22 +98,7 @@ module.exports = opts => {
       return new Promise ( ( resolve, reject ) => {
 
         const spawn = require ( 'child_process' ).spawn;
-        const build = spawn ( relativeBuildDir, [
-          '--connection-timeout=15',
-          '--socket-timeout=15',
-          '--promiscuous-ssl=on',
-          '--name=Ready to check',
-          '--results-title=Showing results',
-          '--page-template=site/NuPageEmitter.xml',
-          '--form-template=site/NuFormEmitter.xml',
-          '--about-file=site/nu-about.html',
-          '--script-file=site/nu-script.js',
-          '--stylesheet-file=site/nu-style.css',
-          'update',
-          'dldeps',
-          'build',
-          data.opts.outputFileType
-        ], {cwd : data.opts.cacheDir} );
+        const build = spawn ( relativeBuildDir, data.opts.buildArguments.concat(data.opts.outputFileType), {cwd : data.opts.cacheDir} );
 
         build.stdout.on ( 'data', d => {
 
