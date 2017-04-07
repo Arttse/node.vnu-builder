c17c49e723934c6dc4042e577d95106011dfa4c6
Nikita Bystrov <arttsesoft@gmail.com>
2017-04-07 17:13:48 +0300

    Bump AVA to ^0.19.0. Small improvement in test

--- test.js
+++ test.js
@@ -7,36 +7,24 @@ import test from 'ava';
 import m from './';
 
 test ( 'Argument opts throws', async t => {
-
   const msg = 'Options must be a plain object';
 
-  t.is ( (await t.throws ( m ( '' ) ) ).message, msg );
-  t.is ( (await t.throws ( m ( 1 ) ) ).message, msg );
-  t.is ( (await t.throws ( m ( true ) ) ).message, msg );
-  t.is ( (await t.throws ( m ( [] ) ) ).message, msg );
-  t.is ( (await t.throws ( m ( null ) ) ).message, msg );
-
+  t.is ( ( await t.throws ( m ( '' ) ) ).message, msg );
+  t.is ( ( await t.throws ( m ( 1 ) ) ).message, msg );
+  t.is ( ( await t.throws ( m ( true ) ) ).message, msg );
+  t.is ( ( await t.throws ( m ( [] ) ) ).message, msg );
+  t.is ( ( await t.throws ( m ( null ) ) ).message, msg );
 } );
 
-test ( 'Build vnu.jar with default options', t => {
-
-  return m ()
-    .then ( result => {
-      t.true ( isPlainObject ( result ) );
-    } );
-
+test ( 'Build vnu.jar with default options', async t => {
+  t.true ( isPlainObject ( await m () ) );
 } );
 
-test ( 'Build vnu.war with many of default options (except 2)', t => {
+test ( 'Build vnu.war with many of default options (except 2)', async t => {
+  const opts = {
+    outputFileType : 'war',
+    cacheDir       : './.cache-vnu-builder-war'
+  };
 
-  return m (
-    {
-      outputFileType : 'war',
-      cacheDir       : './.cache-vnu-builder-war'
-    }
-  )
-    .then ( result => {
-      t.true ( isPlainObject ( result ) );
-    } );
-
+  t.true ( isPlainObject ( await m ( opts ) ) );
 } );
